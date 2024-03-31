999fbdc6fc92ebee84f300a651b81d6af94375d5
IGLESIAS VAZQUEZ yago <yago.iglesias-vazquez@etu.u-paris.fr>
2024-03-31 13:43:53 +0200

    feat: expose implementation details on Implementation module

--- lib/interpreter.mli
+++ lib/interpreter.mli
@@ -1,3 +1,11 @@
 open Ast
 
 val eval : program -> unit
+
+module Implementation : sig
+  type env
+
+  val eval_env : program -> env
+  val value_of : variable -> env -> int
+  val is_correct_value : variable -> int -> env -> bool
+end
