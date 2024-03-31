f8016fa5ea69cc8c02ed14590807c6822cedade1
IGLESIAS VAZQUEZ yago <yago.iglesias-vazquez@etu.u-paris.fr>
2024-03-31 11:56:01 +0200

    test: test expressions

--- test/test_parser.ml
+++ test/test_parser.ml
@@ -11,21 +11,120 @@ let parse s =
 let program_test_case name s expected =
   let actual = parse s in
   test_case name `Quick (fun () ->
-      check (option program_testable) "same program" (Some expected) actual)
+      check (option program_testable) "same program" expected actual)
+
+let option_instr_test_case name s expected =
+  let program =
+    Option.map (fun expected -> [ { number = 0; instr = expected } ]) expected
+  in
+  program_test_case name ("0 " ^ s ^ "\n") program
+
+let fail_instr_test_case name s = option_instr_test_case name s None
 
 let instr_test_case name s expected =
-  let program = [ { number = 0; instr = expected } ] in
-  program_test_case name ("0 " ^ s ^ "\n") program
+  option_instr_test_case name s (Some expected)
 
 let () =
   run "Parser"
     [
-      ( "sum",
+      ( "Assign",
+        [ instr_test_case "simple integer" "X = 1" (Assign ('X', Number 1)) ] );
+      ( "Unary operations",
         [
-          instr_test_case "simple integer" "X = 1" (Assign ('X', Number 1));
+          instr_test_case "positive integer alone" "X = +1"
+            (Assign ('X', Unop (Pos, Number 1)));
+          instr_test_case "negative integer alone" "X = -1"
+            (Assign ('X', Unop (Neg, Number 1)));
+        ] );
+      ( "Sum",
+        [
           instr_test_case "assign with expression" "X = 1 + 2"
             (Assign ('X', Binop (Add, Number 1, Number 2)));
           instr_test_case "assign with expression" "X = 1 + 2 + 3"
             (Assign ('X', Binop (Add, Binop (Add, Number 1, Number 2), Number 3)));
+        ] );
+      ( "Sub",
+        [
+          instr_test_case "assign with expression" "X = 1 - 2"
+            (Assign ('X', Binop (Sub, Number 1, Number 2)));
+          instr_test_case "assign with expression" "X = 1 - 2 - 3"
+            (Assign ('X', Binop (Sub, Binop (Sub, Number 1, Number 2), Number 3)));
+        ] );
+      ( "Mul",
+        [
+          instr_test_case "assign with expression" "X = 1 * 2"
+            (Assign ('X', Binop (Mul, Number 1, Number 2)));
+          instr_test_case "assign with expression" "X = 1 * 2 * 3"
+            (Assign ('X', Binop (Mul, Binop (Mul, Number 1, Number 2), Number 3)));
+        ] );
+      ( "Div",
+        [
+          instr_test_case "assign with expression" "X = 1 / 2"
+            (Assign ('X', Binop (Div, Number 1, Number 2)));
+          instr_test_case "assign with expression" "X = 1 / 2 / 3"
+            (Assign ('X', Binop (Div, Binop (Div, Number 1, Number 2), Number 3)));
+        ] );
+      ( "Integer operations precedence",
+        [
+          instr_test_case "assign with expression" "X = 1 + 2 * 3"
+            (Assign ('X', Binop (Add, Number 1, Binop (Mul, Number 2, Number 3))));
+          instr_test_case "assign with expression" "X = 1 * 2 + 3"
+            (Assign ('X', Binop (Add, Binop (Mul, Number 1, Number 2), Number 3)));
+          instr_test_case "assign with expression" "X = 1 * 2 + 3 * 4"
+            (Assign
+               ( 'X',
+                 Binop
+                   ( Add,
+                     Binop (Mul, Number 1, Number 2),
+                     Binop (Mul, Number 3, Number 4) ) ));
+          instr_test_case "assign with expression" "X = 1 + 2 * 3 + 4"
+            (Assign
+               ( 'X',
+                 Binop
+                   ( Add,
+                     Binop (Add, Number 1, Binop (Mul, Number 2, Number 3)),
+                     Number 4 ) ));
+        ] );
+      ( "Parenthesis in binary operations",
+        [
+          instr_test_case "assign with expression" "X = (1 + 2) * 3"
+            (Assign ('X', Binop (Mul, Binop (Add, Number 1, Number 2), Number 3)));
+          instr_test_case "assign with expression" "X = 1 * (2 + 3)"
+            (Assign ('X', Binop (Mul, Number 1, Binop (Add, Number 2, Number 3))));
+          instr_test_case "assign with expression" "X = (1 + 2) * (3 + 4)"
+            (Assign
+               ( 'X',
+                 Binop
+                   ( Mul,
+                     Binop (Add, Number 1, Number 2),
+                     Binop (Add, Number 3, Number 4) ) ));
+          instr_test_case "assign with expression" "X = (1 + 2) * 3 + 4"
+            (Assign
+               ( 'X',
+                 Binop
+                   ( Add,
+                     Binop (Mul, Binop (Add, Number 1, Number 2), Number 3),
+                     Number 4 ) ));
+          instr_test_case "assign with expression" "X = 1 + (2 * 3) + 4"
+            (Assign
+               ( 'X',
+                 Binop
+                   ( Add,
+                     Binop (Add, Number 1, Binop (Mul, Number 2, Number 3)),
+                     Number 4 ) ));
+        ] );
+      ( "Parenthesis in unary operations",
+        [
+          instr_test_case "assign with expression" "X = -(1 + 2)"
+            (Assign ('X', Unop (Neg, Binop (Add, Number 1, Number 2))));
+          instr_test_case "assign with expression" "X = +(1 + 2)"
+            (Assign ('X', Unop (Pos, Binop (Add, Number 1, Number 2))));
+        ] );
+      ( "REM",
+        [
+          instr_test_case "assign with expression" "REM \"yes\"" (Rem "yes");
+          fail_instr_test_case "assign with expression" "REM \"yes\" \"no\"";
+          fail_instr_test_case "assign with expression" "REM yes";
+          fail_instr_test_case "assign with expression" "REM 1";
         ] );
     ]
