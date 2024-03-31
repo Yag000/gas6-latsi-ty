1c40a02c05c687bf4ad79f2aa8a4c1a927984e89
IGLESIAS VAZQUEZ yago <yago.iglesias-vazquez@etu.u-paris.fr>
2024-03-31 13:49:19 +0200

    feat: simplify main function

--- bin/main.ml
+++ bin/main.ml
@@ -1,11 +1,7 @@
 open Latsi
 open Latsi.Ast
 
-let lexbuf = Lexing.from_channel stdin
-
 let () =
-  Format.printf "Parsing...@.";
+  let lexbuf = Lexing.from_channel stdin in
   let ast = Parser.input Lexer.lexer lexbuf in
-  Format.printf "Parsed@.";
-  if ast = [] then Format.printf "No program found@."
-  else Format.printf "%a" pp_program ast
+  Format.printf "%a" pp_program ast
