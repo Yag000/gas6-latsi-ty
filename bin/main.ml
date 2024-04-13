5b103c044d603ecee0d675e9e055723b0ffd97d0
IGLESIAS VAZQUEZ yago <yago.iglesias-vazquez@etu.u-paris.fr>
2024-04-13 21:51:51 +0200

    feat: Main now reads a file

--- bin/main.ml
+++ bin/main.ml
@@ -2,6 +2,9 @@ open Latsi
 open Latsi.Interpreter
 
 let () =
-  let lexbuf = Lexing.from_channel stdin in
+  let open Latsi.Interpreter.Implementation in
+  let input_file = Sys.argv.(1) in
+  let input_channel = open_in input_file in
+  let lexbuf = Lexing.from_channel input_channel in
   let ast = Parser.input Lexer.lexer lexbuf in
   eval ast
