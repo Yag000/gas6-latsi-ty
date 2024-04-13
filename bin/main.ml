c3337429533ce70ce5cc3a4f320c8260e630f13d
IGLESIAS VAZQUEZ yago <yago.iglesias-vazquez@etu.u-paris.fr>
2024-04-14 00:04:19 +0200

    fix: remove testing import

--- bin/main.ml
+++ bin/main.ml
@@ -2,7 +2,6 @@ open Latsi
 open Latsi.Interpreter
 
 let () =
-  let open Latsi.Interpreter.Implementation in
   let input_file = Sys.argv.(1) in
   let input_channel = open_in input_file in
   let lexbuf = Lexing.from_channel input_channel in
